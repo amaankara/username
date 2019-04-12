2026865b98e4db6ec7916957eb0e3fcc0c5e48a1
“Amaan <amaanunited20@gmail.com>
2019-04-12 10:50:17 -0700

    Adds username.sh

--- username.sh
+++ username.sh
@@ -2,11 +2,11 @@
 # username.sh
 # amaan karachiwala
 echo "Enter username: "
-read ZIP
-while echo "$ZIP" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[a-z]{1}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid Zip Code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
-	read ZIP
+	echo "You must enter a valid USERNAME - It must start with a lowercase letter & at least three but no more than 12 characters!"
+	echo "Enter a username: "
+	read USERNAME
 done
 echo "Thank you"
